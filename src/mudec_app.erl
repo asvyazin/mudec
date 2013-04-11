c6b9e794fde882f1ce1a8a3a34246fbebf357f02
Alexander Svyazin <guybrush@live.ru>
2013-04-11 23:25:32 +0600

    mudec app stub

---
+++ src/mudec_app.erl
@@ -0,0 +1,16 @@
+-module(mudec_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    mudec_sup:start_link().
+
+stop(_State) ->
+    ok.
